6cb83b5a771a0abe848067b010a7e121ef80f7ec
Danielle Bertulfo <danielle.bertulfo@gmail.com>
2024-06-14 23:43:29 -0400

    declare body for each virtual method

--- scripts/State.cs
+++ scripts/State.cs
@@ -6,11 +6,11 @@ public partial class State : Node
     // Public reference to a state machine instance so any state can trigger it to transition to another state
     public StateMachine fsm;
 
-    public virtual void Enter();
-    public virtual void Exit();
+    public virtual void Enter() {};
+    public virtual void Exit() {};
     
-    public virtual void Ready();
-    public virtual void Update(float delta);
-    public virtual void PhysicsUpdate(float delta);
-    public virtual void HandleInput(InputEvent @event);
+    public virtual void Ready() {};
+    public virtual void Update(float delta) {};
+    public virtual void PhysicsUpdate(float delta) {};
+    public virtual void HandleInput(InputEvent @event) {};
 }

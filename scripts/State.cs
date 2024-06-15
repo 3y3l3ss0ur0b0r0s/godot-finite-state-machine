ed422b6a202fa1fe9f11e3e57d522e0726d39fa9
Danielle Bertulfo <danielle.bertulfo@gmail.com>
2024-06-14 23:18:09 -0400

    fix formatting

--- scripts/State.cs
+++ scripts/State.cs
@@ -3,4 +3,14 @@ using System;
 
 public partial class State : Node
 {
+    // Public reference to a state machine instance so any state can trigger it to transition to another state
+    public StateMachine fsm;
+
+    public virtual void Enter();
+    public virtual void Exit();
+    
+    public virtual void Ready();
+    public virtual void Update(float delta);
+    public virtual void PhysicsUpdate(float delta);
+    public virtual void HandleInput(InputEvent @event);
 }

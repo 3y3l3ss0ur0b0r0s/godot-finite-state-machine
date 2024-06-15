a8d1930b995d3211b37b114d0086a05aa1860e8d
Danielle Bertulfo <danielle.bertulfo@gmail.com>
2024-06-14 23:00:59 -0400

    setup StateMachine.cs script for FSM node

--- StateMachine.cs
+++ StateMachine.cs
@@ -3,4 +3,23 @@ using System;
 
 public partial class StateMachine : Node
 {
+    /*
+        Called when node initially enters scene
+        param: N/A
+        return: N/A
+    */
+    public override void _Ready()
+    {
+
+    }
+
+    /*
+        Called every frame
+        param: Elapsed time as double delta
+        return: N/A
+    */
+    public override void _Process(double delta)
+    {
+   
+    }
 }
